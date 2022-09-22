3826f115a1c372059714a636be2167ee6cce2041
Rye Pasaporte <ryepassport@gmail.com>
2022-09-23 00:40:32 +0800

    chore(feat): define person stack construct

---
+++ lib/PersonStack.ts
@@ -0,0 +1,35 @@
+import { PersonTable } from '@constructs/table';
+import { Stack, StackProps } from 'aws-cdk-lib'
+import { Construct } from 'constructs';
+
+/**
+ * Unique props for Person Stack
+ */
+export interface UniquePersonStackProps {
+  /**
+   * Stage name for api gateway used for person api
+   */
+  apiStageName: string
+  /**
+   * Name of table used for Person stack
+   */
+  tableName: string
+}
+
+/**
+ * Person stack type used for the stack definition
+ */
+export interface PersonStackProps extends StackProps, UniquePersonStackProps { }
+
+/**
+ * Main person api infrastructure stack
+ */
+export class PersonStack extends Stack {
+  constructor(scope: Construct, id: string, props: PersonStackProps) {
+    super(scope, id, props)
+  
+    new PersonTable(this, 'PersonTable', {
+      tableName: props.tableName
+    })
+  }
+}
