f100af1a3c7a936e7114a379ec4ccd07b44c5213
Rye Pasaporte <ryepassport@gmail.com>
2022-09-23 00:35:12 +0800

    chore: initialize whole application construct

---
+++ bin/stack.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import 'source-map-support/register'
+
+import { App } from 'aws-cdk-lib'
+import { PersonStack } from '@lib/PersonStack'
+import { config } from './config'
+
+const app = new App()
+
+new PersonStack(app, 'person-infra-app', {
+  stackName: 'person-infra',
+  env: {
+    account: config.account,
+    region: config.region
+  },
+  apiStageName: 'dev'
+})
