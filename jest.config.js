4d4be617917dcf0a804fb91c6e2e0ecbe0e7bd65
Rye Pasaporte <ryepassport@gmail.com>
2022-09-23 00:30:49 +0800

    chore(feat): setup jest settings

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,27 @@
 module.exports = {
-  testEnvironment: 'node',
   roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
+  moduleNameMapper: {
+    '@lib/(.*)$': '<rootDir>/lib/$1',
+    '@test/(.*)$': '<rootDir>/test/$1',
+    '@bin/(.*)$': '<rootDir>/bin/$1'
+  },
+  testPathIgnorePatterns: ['/node_modules/'],
+  testMatch: ['**/*.spec.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
-  }
-};
+  },
+  moduleFileExtensions: ['ts', 'js'],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 80,
+      lines: 80,
+      statements: 90
+    }
+  },
+  collectCoverageFrom: [
+    'lib/**/*.ts',
+  ],
+  verbose: true,
+  reporters: ['default', ['jest-junit', { outputDirectory: 'coverage/test-reports' }]]
+}
