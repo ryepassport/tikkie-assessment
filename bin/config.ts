505c6ec5b4677d5d60bc21017150df51162ff8f8
Rye Pasaporte <ryepassport@gmail.com>
2022-09-23 00:38:16 +0800

    chore: remove table config in bin util

--- bin/config.ts
+++ bin/config.ts
@@ -13,23 +13,9 @@ interface AWSAccount {
 }
 
 /**
- * Table settings
- */
-interface Table {
-  /**
-   * Name of table where data will be stored
-   */
-  tableName: string
-}
-
-/**
  * Main config file for aws account settings
  */
 export const config: AWSAccount = {
   account: '778018686861',
   region: 'ap-southeast-1'
-}
-
-export const table: Table = {
-  tableName: 'persons'
 }
