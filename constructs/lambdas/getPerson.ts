c2ea0b5d2db5e1444f521b4b48ba7c3febadf426
Rye Pasaporte <ryepassport@gmail.com>
2022-09-24 07:00:19 +0800

    chore: define get lambda construct

---
+++ constructs/lambdas/getPerson.ts
@@ -0,0 +1,37 @@
+import { LambdaBaseProps, nodeJSFunctionProps } from '@constructs/lambda'
+import { all, individual } from '@functions/getPerson'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { Construct } from 'constructs'
+
+/**
+ * Request type for fetching data
+ */
+export type GetPersonRequestType = 'LIST' | 'GET'
+
+/**
+ * Unique interface used for fetching persons lambda
+ */
+export interface GetPersonProps extends LambdaBaseProps {
+  /**
+   * See interface definition
+   */
+  requestType: GetPersonRequestType
+}
+
+/**
+ * Construct for get lambda function
+ */
+export class GetPersonLambda extends NodejsFunction {
+  constructor(scope: Construct, id: string, props: GetPersonProps) {
+    const { entry, ...apiGatewayIntegration } = props.requestType === 'LIST' ? all : individual
+
+    super(scope, id, {
+      ...nodeJSFunctionProps,
+      entry
+    })
+
+    props.table.grant(this, 'dynamodb:Get*', 'dynamodb:Query')
+
+    props.apiGateway.addLambdaIntegration(this, apiGatewayIntegration)
+  }
+}
