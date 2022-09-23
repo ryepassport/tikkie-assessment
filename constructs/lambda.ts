1f2787e089866869c951091f7490d9d585a54122
Rye Pasaporte <ryepassport@gmail.com>
2022-09-23 21:36:29 +0800

    chore: add lambda base construct props

---
+++ constructs/lambda.ts
@@ -0,0 +1,40 @@
+import { Duration } from 'aws-cdk-lib'
+import { Architecture, LambdaInsightsVersion, Runtime, Tracing } from 'aws-cdk-lib/aws-lambda'
+import { NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { RetentionDays } from 'aws-cdk-lib/aws-logs'
+import { APIGateway } from './api-gateway'
+import { PersonTable } from './table'
+
+/**
+ * Base props to pass to lambda construct
+ */
+export interface LambdaBaseProps {
+  /**
+   * API gateway where lambda is integrated
+   */
+  apiGateway: APIGateway
+  /**
+   * Storage for lambda
+   */
+  table: PersonTable
+}
+
+/**
+ * Base props for nodejs function
+ */
+export const nodeJSFunctionProps: NodejsFunctionProps = {
+  architecture: Architecture.ARM_64,
+  memorySize: 1024,
+  tracing: Tracing.ACTIVE,
+  timeout: Duration.seconds(5),
+  reservedConcurrentExecutions: 2,
+  logRetention: RetentionDays.ONE_WEEK,
+  insightsVersion: LambdaInsightsVersion.VERSION_1_0_119_0,
+  runtime: Runtime.NODEJS_16_X,
+  bundling: {
+    minify: true,
+    externalModules: [
+      'aws-sdk'
+    ]
+  }
+}
