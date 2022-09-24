87d1b98a96809af29ba534239c2136776d0e7e96
Rye Pasaporte <ryepassport@gmail.com>
2022-09-24 23:14:13 +0800

    test: add test case for saving person data

---
+++ test/functions/savePerson/save.spec.ts
@@ -0,0 +1,53 @@
+import * as savePerson from '@functions/savePerson/save'
+import * as personStorageHelper from '@helpers/storage/person'
+import * as eventHelper from '@helpers/event'
+import { Person } from '@models/person'
+
+jest.mock('@helpers/storage/person')
+jest.mock('@helpers/event')
+
+describe('functions/savePerson', () => {
+  afterEach(() => {
+    jest.resetAllMocks()
+    jest.resetAllMocks()
+  })
+
+  describe('save', () => {
+    const person: Person = {
+      id: 'abc123',
+      firstName: 'Jon',
+      lastName: 'Snow',
+      phone: '12345',
+      address: '12345'
+    }
+
+    it('should save a new record', async () => {
+      const savePersonMock = jest.spyOn(personStorageHelper, 'savePerson').mockResolvedValue(person)
+      const addEventMock = jest.spyOn(eventHelper, 'addEvent').mockResolvedValue()
+      
+      const output = await savePerson.save({
+        data: {} as unknown as Person
+      })
+
+      expect(output).toEqual(person)
+      expect(savePersonMock).toBeCalled()
+      expect(addEventMock).toBeCalled()
+    })
+
+    it('should update a record', async () => {
+      const updatePersonMock = jest.spyOn(personStorageHelper, 'updatePerson').mockResolvedValue(person)
+      const addEventMock = jest.spyOn(eventHelper, 'addEvent').mockResolvedValue()
+
+      const output = await savePerson.save({
+        data: {} as unknown as Person,
+        id: 'abc123'
+      })
+
+      expect(output).toEqual(person)
+      expect(updatePersonMock).toBeCalled()
+      expect(addEventMock).toBeCalled()
+
+    })
+
+  })
+})
