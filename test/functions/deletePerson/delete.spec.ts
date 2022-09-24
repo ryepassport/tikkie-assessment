1e7040b4e68348e8f0db98163dbb9aaa46ce88de
Rye Passport <ryepassport@gmail.com>
2022-09-24 23:40:05 +0800

    test: add test case for delete record (#11)

---
+++ test/functions/deletePerson/delete.spec.ts
@@ -0,0 +1,25 @@
+import * as personStorageHelper from '@helpers/storage/person'
+import * as eventHelper from '@helpers/event'
+import * as deletePerson from '@functions/deletePerson/delete'
+
+jest.mock('@helpers/storage/person')
+jest.mock('@helpers/event')
+
+describe('functions/deletePerson', () => {
+  afterEach(() => {
+    jest.resetAllMocks()
+    jest.resetAllMocks()
+  })
+
+  describe('delete', () => {
+    it('should remove a record', async () => {
+      const deleteMock = jest.spyOn(personStorageHelper, 'removePerson').mockResolvedValue()
+      const addEventMock = jest.spyOn(eventHelper, 'addEvent').mockResolvedValue()
+
+      await deletePerson.remove('banana')
+      
+      expect(deleteMock).toBeCalled()
+      expect(addEventMock).toBeCalled()
+    })
+  })
+})
