94cad83f608f8da33c1f0c1d6f358414a2288db9
Rye Pasaporte <ryepassport@gmail.com>
2022-09-25 15:03:11 +0800

    chore: update stack to use dynamic deployment

--- bin/stack.ts
+++ bin/stack.ts
@@ -2,19 +2,23 @@
 import 'source-map-support/register'
 
 import { App } from 'aws-cdk-lib'
-import { PersonStack } from '@lib/PersonStack'
-import { config } from './config'
-import { QUEUE, TABLE } from '@constants/aws'
+import { PersonStack, UniquePersonStackProps } from '@lib/PersonStack'
+import { Stage } from '@models/aws'
+import { AWS_STAGES, PERSON_STACK_PROPS } from './util'
 
 const app = new App()
 
-new PersonStack(app, 'person-infra-app', {
-  stackName: 'person-infra',
-  env: {
-    account: config.account,
-    region: config.region
-  },
-  apiStageName: 'dev',
-  tableName: TABLE.name,
-  queueName: QUEUE.name
+Object.entries(PERSON_STACK_PROPS).forEach(([key, value]: [string, UniquePersonStackProps]) => {
+  const defaultProps = AWS_STAGES[key as Stage]
+  const { stage, account, region } = defaultProps
+
+  new PersonStack(app, `person-${stage}`, {
+    stackName: `person-infra`,
+    description: 'Infrastructure stack for Persons API',
+    env: {
+      account,
+      region
+    },
+    ...value
+  })
 })
